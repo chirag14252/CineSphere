465c97086cf83d2d0940521c34dd3784651166aa
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-09-23 15:41:03 +0530

    add styling in cards

--- src/Components/cards/Card.jsx
+++ src/Components/cards/Card.jsx
@@ -4,11 +4,13 @@ const Card = (props) =>{
     return(
     <div className="card">
      <div className="movie-img">
-      <img  src ={`https://image.tmdb.org/t/p/w220_and_h330_face/${props.details.backdrop_path}`} alt ="" />
+      <button className="add_fav_button"><i class="fa-solid fa-plus" style={{borderRadius:"30px"}}></i></button>
+      <div className="play_button"><i class="fa-solid fa-play fa-2xl"></i></div>
+      <img  src ={`https://image.tmdb.org/t/p/w220_and_h330_face/${props.details.backdrop_path}`} alt ="" className="img-rendered"/>
      </div>
       <div className="card-body">
       <h2 className="movie-title">{props.details.title?props.details.title:props.details.name}</h2>
-      <h2 className="movie-year">{}</h2>
+      <h2 className="movie-title"><i class="fa-solid fa-star" style={{color:"black"}}></i>{props.details.title?props.details.vote_average:props.details.vote_average}</h2>
       </div>
     </div>
     
